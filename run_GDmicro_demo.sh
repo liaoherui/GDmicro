01bfa7261dd65ad3329f7bb7724ff7a07f619339
Ray <851836818@qq.com>
2023-04-10 13:04:44 +0800

    Update run_GDmicro_demo.sh

--- run_GDmicro_demo.sh
+++ run_GDmicro_demo.sh
@@ -1,20 +1,20 @@
-python GDmicro.py -i Input_file/CRC_10fold.csv -s 10  -t 1  -d CRC -o GDmicro_new_pipe/CRC_10fold
+python GDmicro.py -i Input_files/CRC_10fold.csv -t 1  -d CRC -o GDmicro_new_pipe/CRC_10fold
 
-python GDmicro.py -i Input_file/IBD_10fold.csv -s 10  -t 1  -d IBD -o GDmicro_new_pipe/IBD_10fold
+python GDmicro.py -i Input_files/IBD_10fold.csv -t 1  -d IBD -o GDmicro_new_pipe/IBD_10fold
 
-python GDmicro.py -i Input_file/Obt_10fold.csv -s 10  -t 1  -d Obt -o GDmicro_new_pipe/Obt_10fold
+python GDmicro.py -i Input_files/Obt_10fold.csv -t 1  -d Obt -o GDmicro_new_pipe/Obt_10fold
 
-python GDmicro.py -i Input_file/T2D_10fold.csv -s 10  -t 1  -d T2D -o GDmicro_new_pipe/T2D_10fold
+python GDmicro.py -i Input_files/T2D_10fold.csv -t 1  -d T2D -o GDmicro_new_pipe/T2D_10fold
 
-python GDmicro.py -i Input_file/cirrhosis_10fold.csv -s 10  -t 1  -d cirrhosis -o GDmicro_new_pipe/cirrhosis_10fold
+python GDmicro.py -i Input_files/cirrhosis_10fold.csv -t 1  -d cirrhosis -o GDmicro_new_pipe/cirrhosis_10fold
 
 
-python GDmicro.py -i Input_file/CRC_FRA_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_FRA_LOSO
+python GDmicro.py -i Input_files/CRC_FRA_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_FRA_LOSO
 
-python GDmicro.py -i Input_file/CRC_GER_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_GER_LOSO
+python GDmicro.py -i Input_files/CRC_GER_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_GER_LOSO
 
-python GDmicro.py -i Input_file/CRC_CHI_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_CHI_LOSO
+python GDmicro.py -i Input_files/CRC_CHI_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_CHI_LOSO
 
-python GDmicro.py -i Input_file/CRC_USA_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_USA_LOSO
+python GDmicro.py -i Input_files/CRC_USA_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_USA_LOSO
 
-python GDmicro.py -i Input_file/CRC_AUS_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_AUS_LOSO
+python GDmicro.py -i Input_files/CRC_AUS_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_AUS_LOSO
