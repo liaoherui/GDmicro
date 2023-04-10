0ab1675c2696838661291e2ec758b6992fb9175b
Ray <851836818@qq.com>
2023-04-10 12:18:02 +0800

    Add files via upload

--- run_GDmicro_demo.sh
+++ run_GDmicro_demo.sh
@@ -1,17 +1,20 @@
-###################### Data preprocess part ############################
+python GDmicro.py -i Input_file/CRC_10fold.csv -s 10  -t 1  -d CRC -o GDmicro_new_pipe/CRC_10fold
 
-python GDmicro_preprocess.py -i Test_datasets/Input_train -b  Test_datasets/Input_test -o GDmicro_test_demo/Datasets/Merge_datasets  -d IBD 
+python GDmicro.py -i Input_file/IBD_10fold.csv -s 10  -t 1  -d IBD -o GDmicro_new_pipe/IBD_10fold
 
+python GDmicro.py -i Input_file/Obt_10fold.csv -s 10  -t 1  -d Obt -o GDmicro_new_pipe/Obt_10fold
 
-python GDmicro_preprocess.py -i Test_datasets/Input_train -t 1 -o GDmicro_test_demo/Datasets/Merge_datasets_train_mode -d IBD
+python GDmicro.py -i Input_file/T2D_10fold.csv -s 10  -t 1  -d T2D -o GDmicro_new_pipe/T2D_10fold
 
-#######################  Prediction and 10-fold validation part ###########################
-
-
-python GDmicro.py -i GDmicro_test_demo/Datasets/Merge_datasets -d IBD -o  GDmicro_test_demo/Test_prediction_result/GDmicro_res
+python GDmicro.py -i Input_file/cirrhosis_10fold.csv -s 10  -t 1  -d cirrhosis -o GDmicro_new_pipe/cirrhosis_10fold
 
 
-python GDmicro.py -i GDmicro_test_demo/Datasets/Merge_datasets_train_mode -t 1 -d IBD -o GDmicro_test_demo/Test_validation_result/GDmicro_res
+python GDmicro.py -i Input_file/CRC_FRA_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_FRA_LOSO
 
+python GDmicro.py -i Input_file/CRC_GER_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_GER_LOSO
 
+python GDmicro.py -i Input_file/CRC_CHI_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_CHI_LOSO
 
+python GDmicro.py -i Input_file/CRC_USA_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_USA_LOSO
+
+python GDmicro.py -i Input_file/CRC_AUS_LOSO.csv -s 10  -d CRC -o GDmicro_new_pipe/CRC_AUS_LOSO
